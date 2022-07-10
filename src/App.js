5c863f73dae768ae3a057b9920d6052351ad553b
Chris L <chris.ladden@gmail.com>
2022-07-10 11:16:56 -0500

    added search functionality and styling

--- src/App.js
+++ src/App.js
@@ -2,34 +2,43 @@ import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                setData(resData.results)
-            } else {
-                setMessage('Not Found')
-            }
-        }
-        fetchData()
-    }, [search])
+  const App = () => {
+        let [search, setSearch] = useState('')
+        let [message, setMessage] = useState('Search for Music!')
+        let [data, setData] = useState([])
     
-
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
-}
-
-export default App
+        const API_URL = 'https://itunes.apple.com/search?term='
+    
+        useEffect(() => {
+            if(search) {
+                const fetchData = async () => {
+                    document.title = `${search} music`
+                    const response = await fetch(API_URL + search)
+                    const resData = await response.json()
+                    if (resData.results.length > 0) {
+                        return setData(resData.results)
+                    } else {
+                        return setMessage('Not Found.')
+                    }
+                }
+                fetchData()
+            }
+        }, [search])
+    
+        const handleSearch = (e, term) => {
+            e.preventDefault()
+            setSearch(term)
+        }
+    
+        return (
+            <div>
+                <SearchBar handleSearch={handleSearch} />
+                {message}
+                <Gallery data={data} />
+            </div>
+        )
+    }
+    
+    export default App
+    
 
