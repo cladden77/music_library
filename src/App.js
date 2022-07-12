c4e3263ab292375406cd8a903c4cdd89545cfdd1
Chris L <chris.ladden@gmail.com>
2022-07-12 18:30:04 -0500

    added context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+
 
   const App = () => {
         let [search, setSearch] = useState('')
@@ -34,7 +36,9 @@ import SearchBar from './components/SearchBar'
             <div>
                 <SearchBar handleSearch={handleSearch} />
                 {message}
-                <Gallery data={data} />
+                <DataContext.Provider value={data}>
+                <Gallery />
+                </DataContext.Provider>
             </div>
         )
     }
