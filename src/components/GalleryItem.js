d92056be72ece45dfecdfbb9d228e4b84db0adb4
Chris L <chris.ladden@gmail.com>
2022-07-14 16:47:14 -0500

    completed search functionality and album-artist views

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react' 
+import { Link } from 'react-router-dom'
 
+function GalleryItem(props){
+    let [view, setView] = useState(false)
 
-function GalleryItem(props) {
     const simpleStyle = {
         'width': '25vw',
         'height': '20vh',
@@ -20,9 +22,6 @@ function GalleryItem(props) {
         'color': 'yellow'
     }
 
-    
-    let [view, setView] = useState(false)
-
     const simpleView = () => {
         return (
             <div style={simpleStyle}>
@@ -36,7 +35,16 @@ function GalleryItem(props) {
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
@@ -44,14 +52,11 @@ function GalleryItem(props) {
     }
 
     return (
-        <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
-        
+        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
             {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
-
         </div>
     )
-
 }
+
 export default GalleryItem
